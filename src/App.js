d3a5c006ed8b61d77b89c809a705c1912c93fb4c
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-03-14 22:55:22 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
+      <Router>
+        <Routes>
+
+        </Routes>
+      </Router>
     </div>
   );
 }
