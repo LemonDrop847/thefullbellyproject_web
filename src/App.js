7ea73f26decdae134667ce3fe5d33ba7e5dc139e
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-03-16 22:44:23 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <NavBar/>
       <Router>
         <Routes>
-
+          <Route path='/' element={<Home/>}/>
         </Routes>
       </Router>
     </div>
