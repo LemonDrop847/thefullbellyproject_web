978353ce81d54ec828493791e0c1f33a1a7437d2
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-03-22 20:01:58 +0530

    kuch bhi

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/Navbar';
-import Home from './components/Home';
+import Home from './pages/Home';
 
 function App() {
   return (
