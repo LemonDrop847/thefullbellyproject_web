f08e3fd0543d2c44787b3ec9890889e0dd3f518c
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-03-25 16:04:28 +0530

    Home Structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,121 @@
 const Home = () => {
     return ( 
-        <div className="row">
-            <div className="col">
-                <h1>OUR MISSOION</h1>
-                <p>
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
-                </p>
-                <button className="btn btn-danger">Learn More</button>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h1>OUR MISSOION</h1>
+                    <p>
+                    Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+                    </p>
+                    <button className="btn btn-danger">Learn More</button>
+                </div>
+                <div className="col">
+                    <img style={{height:"350px"}} src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
+                </div>
             </div>
-            <div className="col"></div>
+            <div className="row">
+                <h2>Photo Gallery</h2>
+                <h4>See the smile brought by your attention</h4>
+                <div className="col">
+                    <img style={{height:"200px"}} src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
+                    <br />
+                    <img style={{height:"200px"}} src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
+                    </div>
+                    <div className="col">
+                    <img style={{height:"200px"}} src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
+                    <br />
+                    <img style={{height:"200px"}} src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <h1>APPLY TO BE A  HERO</h1>
+                    <p>
+                    Do you want to make a genuine impact in your community? Join our charity volunteer squad and help us bring about positive change in underserved areas.                    </p>
+                    <button className="btn btn-danger">As NGOs</button>
+                    <button className="btn btn-danger">As individual</button>
+                </div>
+                <div className="col">
+                    <img style={{height:"350px"}} src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <img style={{height:"350px"}} src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
+                </div>
+                <div className="col">
+                    <h1>HOW FULLBELLY APP WORKS </h1>
+                    <p>
+                    Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+                    </p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <h1>FULLBELLY Application  </h1>
+                    <p>
+                    Designed with users in mind, "FullBelly" is an initiative, easy-to-use application. Here are the steps to use "FullBelly" for food procurement and donation
+                    <br/>
+
+1.Enter the App and Login 
+<br/>
+2. Fill up profile
+<br/>
+3. You can now donate food / Procure food for    distribution    
+<br/>                </p>
+                </div>
+                <div className="col">
+                    <img style={{height:"350px"}} src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <h1>News and Updates</h1>
+                </div>
+                <div className="col">
+                <p>
+
+                    <strong>Stay informed with the latest developments on charity campaigns to
+keep you engaged.</strong>
+<br />
+<button>See all news</button>
+                </p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+
+                    <img style={{height:"100px"}} src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
+                    <p>Jan 03,2023</p>
+                    <h3>Food from Hotels will go straight to the needy</h3>
+                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Exercitation veniam .</p>
+                    <button>Read More</button>
+                </div>
+                <div className="col">
+
+                    <img style={{height:"100px"}} src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
+                    <p>Jan 03,2023</p>
+                    <h3>Food from Hotels will go straight to the needy</h3>
+                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Exercitation veniam .</p>
+                    <button>Read More</button>
+                </div>
+                <div className="col">
+
+                    <img style={{height:"100px"}} src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
+                    <p>Jan 03,2023</p>
+                    <h3>Food from Hotels will go straight to the needy</h3>
+                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Exercitation veniam .</p>
+                    <button>Read More</button>
+                </div>
+                <div className="col">
+
+                    <img style={{height:"100px"}} src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
+                    <p>Jan 03,2023</p>
+                    <h3>Food from Hotels will go straight to the needy</h3>
+                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Exercitation veniam .</p>
+                    <button>Read More</button>
+                </div>
+            </div>
         </div>
      );
 }
