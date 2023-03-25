d529e81ec55ccebe19c9fe531ba0314332908fb8
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-03-25 23:15:18 +0530

    carousel added in news

---
+++ src/components/News.jsx
@@ -0,0 +1,199 @@
+import Card from 'react-bootstrap/Card';
+import Carousel from 'react-bootstrap/Carousel';
+
+
+const News = () => {
+    return ( 
+        <Carousel variant="dark">
+            <Carousel.Item interval={3000}>
+            <div className="container">
+                    <div className="row adjust">
+                        <div className="col">
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" />
+                                <Card.Body>
+                                    <Card.Text>Jan 03,2023</Card.Text>
+                                    <Card.Title>Food from Hotels will go straight to the needy</Card.Title>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                    </Card.Text>
+                                    <button className="bttn home-btn">Read More</button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="col">
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" />
+                                <Card.Body>
+                                    <Card.Text>Jan 03,2023</Card.Text>
+                                    <Card.Title>Food from Hotels will go straight to the needy</Card.Title>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                    </Card.Text>
+                                    <button className="bttn home-btn">Read More</button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="col">
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" />
+                                <Card.Body>
+                                    <Card.Text>Jan 03,2023</Card.Text>
+                                    <Card.Title>Food from Hotels will go straight to the needy</Card.Title>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                    </Card.Text>
+                                    <button className="bttn home-btn">Read More</button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="col">
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" />
+                                <Card.Body>
+                                    <Card.Text>Jan 03,2023</Card.Text>
+                                    <Card.Title>Food from Hotels will go straight to the needy</Card.Title>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                    </Card.Text>
+                                    <button className="bttn home-btn">Read More</button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </div>
+                </div>
+            </Carousel.Item>
+            <Carousel.Item interval={3000}>
+            <div className="container">
+                    <div className="row adjust">
+                        <div className="col">
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" />
+                                <Card.Body>
+                                    <Card.Text>Jan 03,2023</Card.Text>
+                                    <Card.Title>Food from Hotels will go straight to the needy</Card.Title>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                    </Card.Text>
+                                    <button className="bttn home-btn">Read More</button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="col">
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" />
+                                <Card.Body>
+                                    <Card.Text>Jan 03,2023</Card.Text>
+                                    <Card.Title>Food from Hotels will go straight to the needy</Card.Title>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                    </Card.Text>
+                                    <button className="bttn home-btn">Read More</button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="col">
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" />
+                                <Card.Body>
+                                    <Card.Text>Jan 03,2023</Card.Text>
+                                    <Card.Title>Food from Hotels will go straight to the needy</Card.Title>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                    </Card.Text>
+                                    <button className="bttn home-btn">Read More</button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="col">
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" />
+                                <Card.Body>
+                                    <Card.Text>Jan 03,2023</Card.Text>
+                                    <Card.Title>Food from Hotels will go straight to the needy</Card.Title>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                    </Card.Text>
+                                    <button className="bttn home-btn">Read More</button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </div>
+                </div>
+            </Carousel.Item>
+            <Carousel.Item interval={3000}>
+            <div className="container">
+                    <div className="row adjust">
+                        <div className="col">
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" />
+                                <Card.Body>
+                                    <Card.Text>Jan 03,2023</Card.Text>
+                                    <Card.Title>Food from Hotels will go straight to the needy</Card.Title>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                    </Card.Text>
+                                    <button className="bttn home-btn">Read More</button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="col">
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" />
+                                <Card.Body>
+                                    <Card.Text>Jan 03,2023</Card.Text>
+                                    <Card.Title>Food from Hotels will go straight to the needy</Card.Title>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                    </Card.Text>
+                                    <button className="bttn home-btn">Read More</button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="col">
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" />
+                                <Card.Body>
+                                    <Card.Text>Jan 03,2023</Card.Text>
+                                    <Card.Title>Food from Hotels will go straight to the needy</Card.Title>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                    </Card.Text>
+                                    <button className="bttn home-btn">Read More</button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="col">
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" />
+                                <Card.Body>
+                                    <Card.Text>Jan 03,2023</Card.Text>
+                                    <Card.Title>Food from Hotels will go straight to the needy</Card.Title>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                    </Card.Text>
+                                    <button className="bttn home-btn">Read More</button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </div>
+                </div>
+            </Carousel.Item>
+        </Carousel>
+        
+     );
+}
+ 
+export default News;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import "./styles/Home.css";
 import Card from 'react-bootstrap/Card';
 import Foots from "../components/Footer";
+import News from "../components/News";
 
 const Home = () => {
     return ( 
@@ -105,65 +106,9 @@ const Home = () => {
                     </p>
                     </div>
                 </div>
-                <div className="row adjust">
-                    <div className="col">
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" />
-                            <Card.Body>
-                                <Card.Text>Jan 03,2023</Card.Text>
-                                <Card.Title>Food from Hotels will go straight to the needy</Card.Title>
-                                <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                                </Card.Text>
-                                <button className="bttn home-btn">Read More</button>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                    <div className="col">
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" />
-                            <Card.Body>
-                                <Card.Text>Jan 03,2023</Card.Text>
-                                <Card.Title>Food from Hotels will go straight to the needy</Card.Title>
-                                <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                                </Card.Text>
-                                <button className="bttn home-btn">Read More</button>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                    <div className="col">
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" />
-                            <Card.Body>
-                                <Card.Text>Jan 03,2023</Card.Text>
-                                <Card.Title>Food from Hotels will go straight to the needy</Card.Title>
-                                <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                                </Card.Text>
-                                <button className="bttn home-btn">Read More</button>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                    <div className="col">
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" />
-                            <Card.Body>
-                                <Card.Text>Jan 03,2023</Card.Text>
-                                <Card.Title>Food from Hotels will go straight to the needy</Card.Title>
-                                <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                                </Card.Text>
-                                <button className="bttn home-btn">Read More</button>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                </div>
+                
             </div>
+            <News/>
             <Foots/>
         </div>
      );
