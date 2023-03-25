10323e85474a0528b46681621c4a4e5c53ea7e80
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-03-25 23:18:13 +0530

    gal img style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,14 +38,14 @@ const Home = () => {
                 <h1>Photo Gallery</h1>
                 <h4>See the smile brought by your attention</h4>
                 <div className="col">
-                    <img style={{height:"200px"}} src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
+                    <img className="galimg" src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
                     <br />
-                    <img style={{height:"200px"}} src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
+                    <img className="galimg" src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
                     </div>
                     <div className="col">
-                    <img style={{height:"200px"}} src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
+                    <img className="galimg" src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
                     <br />
-                    <img style={{height:"200px"}} src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
+                    <img className="galimg" src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
                 </div>
             </div>
             <div className="container">
