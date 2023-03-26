3022ffe8e96ab232e1ebd619162fe14b9ae8aaa0
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-03-26 15:30:53 +0530

    accordation added

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,4 +1,6 @@
 import Card from 'react-bootstrap/Card';
+import { Accordion } from "react-bootstrap";
+import Foots from '../components/Footer';
 
 const About = () => {
     return ( 
@@ -122,10 +124,41 @@ const About = () => {
                         </div>
                     </div>
                 </div>
-                <div className="row">
+                <div className="row adjust" style={{marginBottom:"1rem"}}>
                     <h1>FAQ</h1>
-                    
+                    <div className="App">
+                        <Accordion>
+                            <Accordion.Item eventKey="0">
+                            <Accordion.Header>Accordion Item #1</Accordion.Header>
+                            <Accordion.Body>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                                ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                culpa qui officia deserunt mollit anim id est laborum.
+                            </Accordion.Body>
+                            </Accordion.Item>
+                            <Accordion.Item eventKey="1">
+                            <Accordion.Header>Accordion Item #2</Accordion.Header>
+                            <Accordion.Body>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                                ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                culpa qui officia deserunt mollit anim id est laborum.
+                            </Accordion.Body>
+                            </Accordion.Item>
+                        </Accordion>
+
+                        {/* <h1>Hello CodeSandbox</h1>
+                        <h2>Start editing to see some magic happen!</h2> */}
+                        </div>
                 </div>
+            <Foots/>
             </div>
         </>
      );
