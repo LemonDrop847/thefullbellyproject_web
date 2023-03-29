a3ff8fdd0f9876ba259b19df8cda558d06ca3c0d
LemonDrop847 <nitinmishra847@gmail.com>
2023-03-29 23:02:40 +0530

    Donations added

--- src/pages/Deliver.jsx
+++ src/pages/Deliver.jsx
@@ -1,25 +1,53 @@
+import { useState, useEffect } from "react";
+import { db } from "../components/services/firebase";
+import { onSnapshot, collection, orderBy , query , limit } from "firebase/firestore";
 import Foots from "../components/Footer";
 
 const Deliver = () => {
-    return ( 
-        <>
-            <div className="container">
-                <div className="row adjust">
-                    <div className="col">
-                        <img src="https://i.postimg.cc/Prw6rwpw/a3ee095fdba51b3503f6fa41b811564a.jpg" alt="" />
-                    </div>
-                    <div className="col">
-                        <h2>
-                            Delivered
-                        </h2>
-                        <h3>Location</h3>
-                        <h3>date and time</h3>
-                    </div>
-                </div>
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(
+        collection(db, "transactions"),
+        orderBy("timestamp", "desc"),
+        limit(15)
+      ),
+      (snapshot) => {
+        const newPosts = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        })).slice(0, 15);
+        setPosts(newPosts);
+      }
+    );
+    return unsubscribe;
+  }, []);
+  
+  return (
+    <>
+      {posts.map((post) => (
+        <div className="container" key={post.id}>
+          <div className="row adjust">
+            <div className="col">
+              <img
+                style={{ maxHeight: "200px" }}
+                src={post.imageUrls[0]}
+                alt=""
+              />
             </div>
-            <Foots/>
-        </>
-     );
-}
- 
+            <div className="col">
+              <h3>Fed {post.quantity}</h3>
+              <h3>Location: {post.location}</h3>
+              <h3>At: {post.timestamp.toDate().toDateString()}</h3>
+            </div>
+          </div>
+        </div>
+      ))}
+
+      <Foots />
+    </>
+  );
+};
+
 export default Deliver;
