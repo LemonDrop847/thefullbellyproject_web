d811c1b792a7895c32e8e912f2429f079043e6d1
LemonDrop847 <nitinmishra847@gmail.com>
2023-03-30 09:47:40 +0530

    App link

--- src/components/appButton.jsx
+++ src/components/appButton.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React from "react";
 
 class AppLinkButton extends React.Component {
   handleButtonClick = () => {
     if (navigator.userAgent.match(/(Android|iPhone)/)) {
-      window.location.href = "thefullbellyproject_app://";
+      // window.location.href = "thefullbellyproject_app://";
+      window.location.href =
+        "https://drive.google.com/uc?id=1jsBfZSw9rSznG2Xz_-mJVXq6r-90F5uk&export=download";
     } else {
-      window.location.href = "https://drive.google.com/uc?id=1jsBfZSw9rSznG2Xz_-mJVXq6r-90F5uk&export=download";
+      window.location.href =
+        "https://drive.google.com/uc?id=1jsBfZSw9rSznG2Xz_-mJVXq6r-90F5uk&export=download";
     }
-  }
+  };
 
   render() {
-    return (
-      <button onClick={this.handleButtonClick}>Open My App</button>
-    );
+    return <button onClick={this.handleButtonClick}>Open My App</button>;
   }
 }
 
